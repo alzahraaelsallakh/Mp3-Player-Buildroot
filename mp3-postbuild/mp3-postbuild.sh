8f4683813a651ea7c450fe5acfaa719098c70096
alzahraaelsallakh <zahraaelsallakh@gmail.com>
2020-04-11 19:12:01 +0200

    Running cmd line from profile

--- mp3-postbuild/mp3-postbuild.sh
+++ mp3-postbuild/mp3-postbuild.sh
@@ -7,12 +7,16 @@ output/host/bin/aarch64-linux-gcc myApplications/printHello.c -o ${TARGET_DIR}/m
 
 echo "/myApplications/printHello.o" >> ${TARGET_DIR}/etc/profile
 
+echo "cd /mp3Files" >> ${TARGET_DIR}/etc/profile
+
+echo "./mp3commandline" >> ${TARGET_DIR}/etc/profile
+
 ########################Audio Output Settings##########################################
 
-sudo cp $(dirname $0)/mp3-player/S50-Bluetooth-Daemon-service ${TARGET_DIR}/etc/init.d/
-sudo chmod 777 ${TARGET_DIR}/etc/init.d/S50-Bluetooth-Daemon-service
+#sudo cp $(dirname $0)/mp3-player/S50-Bluetooth-Daemon-service ${TARGET_DIR}/etc/init.d/
+#sudo chmod 777 ${TARGET_DIR}/etc/init.d/S50-Bluetooth-Daemon-service
 
-sudo cp $(dirname $0)/mp3-player/Bluetooth_init ${TARGET_DIR}/root/mp3-player/
-sudo cp $(dirname $0)/mp3-player/audio_ouput ${TARGET_DIR}/root/mp3-player/
+#sudo cp $(dirname $0)/mp3-player/Bluetooth_init ${TARGET_DIR}/root/mp3-player/
+#sudo cp $(dirname $0)/mp3-player/audio_ouput ${TARGET_DIR}/root/mp3-player/
 
-sudo chmod -R 777 ${TARGET_DIR}/root
+#sudo chmod -R 777 ${TARGET_DIR}/root
